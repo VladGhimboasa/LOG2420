367bf060ed3fcd608ae12aaa347802b0b92a775b
Vlad Ghimboasa <vladalexandrughimboasa@gmail.com>
2020-03-08 23:45:20 -0400

    presque fini

---
+++ scriptPage2.js
@@ -0,0 +1,162 @@
+var clicked1=false;
+        var clicked2=false;
+        var clicked3=false;
+        var clicked4=false;
+        var clicked5=false;
+        var clicked6=false;
+        var clicked7=false;
+        var clicked8=false;
+        var clicked9=false;
+        function grossir1(){
+        var x=document.getElementById("etoile1");
+        if (clicked1==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked1=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="rgb(215, 215, 215)";
+        clicked1=false;
+        }
+        }
+        function grossir2(){
+        var x=document.getElementById("etoile2");
+        if (clicked2==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked2=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked2=false;
+        }
+        }
+        function grossir3(){
+        var x=document.getElementById("etoile3");
+        if (clicked3==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked3=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked3=false;
+        }
+        }
+        function grossir4(){
+        var x=document.getElementById("etoile4");
+        if (clicked4==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked4=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked4=false;
+        }
+        }
+        function grossir5(){
+        var x=document.getElementById("etoile5");
+        if (clicked5==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked5=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked5=false;
+        }
+        }
+        function grossir6(){
+        var x=document.getElementById("etoile6");
+        if (clicked6==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked6=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked6=false;
+        }
+        }
+        function grossir7(){
+        var x=document.getElementById("etoile7");
+        if (clicked7==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked7=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked7=false;
+        }
+        }
+        function grossir8(){
+        var x=document.getElementById("etoile8");
+        if (clicked8==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked8=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked8=false;
+        }
+        }
+        function grossir9(){
+        var x=document.getElementById("etoile9");
+        if (clicked9==false)
+        {
+        x.style.color="yellow";
+        x.style.fontSize="40px";
+        x.style.backgroundColor="gray";
+        clicked9=true;
+        }
+        else
+        {
+        x.style.color="black";
+        x.style.fontSize="24px";
+        x.style.backgroundColor="white";
+        clicked9=false;
+        }
+        }
